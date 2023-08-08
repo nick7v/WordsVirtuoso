710e86ceac0093786f0f62af88be90ee8810f3fc
nick7v <125310825+nick7v@users.noreply.github.com>
2023-08-08 23:23:14 +0300

    ver 1.2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,27 +3,33 @@ package wordsvirtuoso
 import java.io.File
 import kotlin.system.exitProcess
 
-class Words() {
-    fun start(args: Array<String>) {
+class Words(private val files: Array<String>) {
+    private val allWordsList = mutableListOf<String>()
+    private var secretWord = ""
+
+    fun start() {
         when {
-            args.size != 2 -> "Error: Wrong number of arguments."
-            !File(args[0]).exists() -> "Error: The words file ${args[0]} doesn't exist."
-            !File(args[1]).exists() -> "Error: The candidate words file ${args[1]} doesn't exist."
-            else -> checkFiles(args[0], args[1])
-        }.let(::println)
+            files.size != 2 -> println("Error: Wrong number of arguments.")
+            !File(files[0]).exists() -> println("Error: The words file ${files[0]} doesn't exist.")
+            !File(files[1]).exists() -> println("Error: The candidate words file ${files[1]} doesn't exist.")
+            else -> checkFiles(files[0], files[1])
+        }
     }
 
-    fun checkFiles(allWords: String, candidatesWords: String): String {
-        val allWordsList = checkInvalidWords(allWords)
-        val candidatesWordsList = checkInvalidWords(candidatesWords).toMutableList()
-
+    private fun checkFiles(allWordsFileName: String, candidatesWordsFileName: String) {
+        allWordsList.addAll(checkInvalidWords(allWordsFileName))
+        val candidatesWordsList = checkInvalidWords(candidatesWordsFileName).toMutableList()
+        secretWord = candidatesWordsList.random().lowercase()
         candidatesWordsList.removeAll(allWordsList)
-        return if (candidatesWordsList.size != 0)
-            "Error: ${candidatesWordsList.size} candidate words are not included in the $allWords file."
-        else "Words Virtuoso"
+        if (candidatesWordsList.size != 0)
+            println("Error: ${candidatesWordsList.size} candidate words are not included in the $allWordsFileName file.")
+        else {
+            println("Words Virtuoso")
+            startGame()
+        }
     }
 
-    fun checkInvalidWords(fileName: String): List<String> {
+    private fun checkInvalidWords(fileName: String): List<String> {
         val listOfWords = File(fileName).readLines().map { it.lowercase() }
         var numberInvalidWords = 0
         listOfWords.forEach {
@@ -37,11 +43,40 @@ class Words() {
         } else listOfWords
     }
 
-    fun startGame () {
-        println("dghf")
+    private fun startGame () {
+        while (true) {
+            println("\nInput a 5-letter word:")
+            with(readln().lowercase()) {
+                if (this == "exit") println("The game is over.").also { return }
+                println(checkInputForErors(this) ?: checkInputIsSecretWord(this))
+            }
+        }
+    }
+
+    private fun checkInputForErors(input: String): String? {
+        return when {
+            input.length != 5 -> "The input isn't a 5-letter word."
+            !Regex("[a-z]+").matches(input) -> "One or more letters of the input aren't valid."
+            input.toSet().size != 5 -> "The input has duplicate letters."
+            input !in allWordsList -> "The input word isn't included in my words list."
+            else -> null
+        }
+    }
+
+    private fun checkInputIsSecretWord (input: String): String {
+        var clue = ""
+        for (index in 0..4) {
+            clue += when {
+                input[index] == secretWord[index] -> input[index].uppercase()
+                input[index] in secretWord -> input[index]
+                else -> "_"
+            }
+        }
+        if (clue.lowercase() == secretWord) println("Correct!").also { exitProcess(1) }
+        return clue
     }
 }
 
 fun main(args: Array<String>) {
-    Words().start(args)
+    Words(args).start()
 }
